2f0bc15c97a0e4351bba289b7cbe70579c5a3727
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2023-06-27 20:05:41 -0300

    top implementado

--- FreeRTOS/CORTEX_LM3S811_GCC/main.c
+++ FreeRTOS/CORTEX_LM3S811_GCC/main.c
@@ -4,7 +4,6 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
-#include "semphr.h"
 
 
 /* Delay between cycles of the 'check' task. */
@@ -17,18 +16,22 @@ efficient. */
 
 /* Misc. */
 #define mainQUEUE_SIZE				( 3 )
-#define SEED 420
+#define SEED 42069
 
 int getRandomValue();
 static void vUARTinit( void );
+void Timer0IntHandler(void);
 
 //TASKS
 //static void vCounterTask( void *pvParameters );
 static void vSensorTask(void *pvParameters);
 static void vFilterTask(void *pvParameters);
 static void vDisplayTask(void *pvParameters);
+static void vTopTask(void *pvParameters);
 
 
+unsigned long ulHighFrequencyTimerTicks;
+TaskStatus_t * pxTaskStatusArray;
 QueueHandle_t xSensorQueue;
 QueueHandle_t xDisplayTempQueue;
 QueueHandle_t xDisplayNQueue;
@@ -48,9 +51,10 @@ int main( void )
 
 	/* Start the tasks defined within the file. */
 	//xTaskCreate( vCounterTask, "Counter", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-	xTaskCreate( vSensorTask, "Sensor", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-	xTaskCreate( vFilterTask, "Filter", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-	xTaskCreate( vDisplayTask, "Display", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
+	xTaskCreate( vSensorTask, "Sensor", configMINIMAL_STACK_SIZE, NULL, 5, NULL );
+	xTaskCreate( vFilterTask, "Filter", configMINIMAL_STACK_SIZE, NULL, 4, NULL );
+	xTaskCreate( vDisplayTask, "Display", configMINIMAL_STACK_SIZE, NULL, 3, NULL );
+    xTaskCreate( vTopTask, "Top", configMINIMAL_STACK_SIZE, NULL, 2, NULL );
 	/* Start the scheduler. */
 	vTaskStartScheduler();
 
@@ -112,6 +116,7 @@ void vSensorTask(void *pvParameters) {
     xLastWakeTime = xTaskGetTickCount();
 	int value = 30;
 	char string[10];
+    uxTaskGetStackHighWaterMark(NULL);
     while (true) {
 		if(getRandomValue()%2==0)value++;
 		else value--;
@@ -134,6 +139,7 @@ void vFilterTask(void *pvParameters){
     int promedio;
     int N=9;
     int suma;
+    uxTaskGetStackHighWaterMark(NULL);
 	while(true){
 		if (xQueueReceive(xUARTQueue, &N, 0) == pdPASS) {    
 			xQueueSend(xDisplayNQueue,&N,portMAX_DELAY);
@@ -159,6 +165,7 @@ static void vDisplayTask(void *pvParameters){
     char image_data[96];
     for(int i=0;i<96;i++) image_data[i]=0xFF;
     char string2[4] = {'N','=','9','\0'};
+    uxTaskGetStackHighWaterMark(NULL);
 	while(true){
         if (xQueueReceive(xDisplayNQueue, &N, 0) == pdPASS) {
 			string2[2]=(char)(N+48);
@@ -173,6 +180,70 @@ static void vDisplayTask(void *pvParameters){
 	}
 }
 
+static void vTopTask(void *pvParameters){
+    const TickType_t xDelay = pdMS_TO_TICKS(10000);
+    UBaseType_t uxArraySize = uxTaskGetNumberOfTasks();
+    pxTaskStatusArray = pvPortMalloc(uxArraySize * sizeof(TaskStatus_t));
+
+    uxTaskGetStackHighWaterMark(NULL);
+    while(true){
+        volatile UBaseType_t uxArraySize;
+        volatile UBaseType_t x;
+
+        unsigned long ulTotalRunTime;
+        unsigned long ulStatsAsPercentage;
+
+        if (pxTaskStatusArray != NULL) {
+            uxArraySize = uxTaskGetSystemState(pxTaskStatusArray, uxArraySize, &ulTotalRunTime);
+
+            ulTotalRunTime /= 100UL;
+
+            UartSend("\r");
+
+            if (ulTotalRunTime > 0) {
+                UartSend("TAREA\t|TICKS\t|CPU%\t|STACK FREE\r\n");
+
+                for (x = 0; x < uxArraySize; x++) {
+                    ulStatsAsPercentage = pxTaskStatusArray[x].ulRunTimeCounter / ulTotalRunTime;
+
+                    char counter[8];
+                    char porcentaje[8];
+                    char stack[8];
+
+                    UartSend(pxTaskStatusArray[x].pcTaskName);
+                    UartSend("\t|");
+                    enteroToString(pxTaskStatusArray[x].ulRunTimeCounter, counter);
+                    UartSend(counter);
+                    UartSend("\t|");
+
+                    if (ulStatsAsPercentage > 0UL) {
+                    enteroToString(ulStatsAsPercentage, porcentaje);
+                    UartSend(porcentaje);
+                    } else {
+                    UartSend("0");
+                    }
+
+                    UartSend("%\t|");
+                    enteroToString(pxTaskStatusArray[x].usStackHighWaterMark, stack);
+                    UartSend(stack);
+                    UartSend(" Words\r\n");
+                }
+
+                    UartSend("\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n");
+                }
+            }
+            vTaskDelay(xDelay);
+    }
+
+}
+
+void UartSend(const char *string) {
+  while(*string != '\0') {
+    UARTCharPut(UART0_BASE, *string++);
+  }
+
+  UARTCharPut(UART0_BASE, '\0');
+}
 //algoritmo generador de números pseudoaleatorios conocido como "congruencia lineal"
 int getRandomValue()
 {
@@ -215,3 +286,22 @@ void enteroToString(int numero, char *cadena) {
     cadena[contador] = '\0';
 }
 
+unsigned long obtenerValor(void) {
+  return ulHighFrequencyTimerTicks;
+}
+
+void configurarTimer0(void) {
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+  IntMasterEnable();
+  TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+  TimerConfigure(TIMER0_BASE, TIMER_CFG_32_BIT_TIMER);
+  TimerLoadSet(TIMER0_BASE, TIMER_A, 30000);
+  TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);
+  TimerEnable(TIMER0_BASE, TIMER_A);
+}
+
+void Timer0IntHandler(void) {
+  TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+  ulHighFrequencyTimerTicks++;
+}
